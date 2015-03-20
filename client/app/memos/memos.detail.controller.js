06a9f3c1db216cca538333bd9fa08b4c4db5d25c
Palsskv <palsskv@gmail.com>
2015-03-20 16:24:56 +0100

    Added growl notifications

--- client/app/memos/memos.detail.controller.js
+++ client/app/memos/memos.detail.controller.js
@@ -1,12 +1,22 @@
 'use strict';
 
 angular.module('seminarJsApp')
-  .controller('MemosDetailCtrl', function ($stateParams, $scope, $location, $state, Memos) {
+  .controller('MemosDetailCtrl', function ($stateParams, $scope, $location, $state, Memos, growl) {
       $scope.memo = {};
 
     	$scope.save = function () {
-        	var cb = function(){$state.go('memos.list');};
-          
-	        Memos.save($scope.memo, cb);
+        	var successCallback = function(){ 
+                $state.go('memos.list');
+          };
+          var failureCallback = function(response)
+          {
+            console.log(response);
+            if(response.data.errors)
+            {
+              var message = _.pluck(response.data.errors,'message').join(",")
+              growl.error(message);
+            }
+          };
+	        Memos.save($scope.memo, successCallback, failureCallback);
 		};
     });
